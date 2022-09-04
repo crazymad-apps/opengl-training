09ef889d8a74dc70891e587632ff21c01324bada
crazymad-apps <crazymad0601.apps@gmail.com>
2022-09-04 22:42:43 +0800

    camera fixed view

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ implement_vertex!(Vertex, position, tex_coords);
 fn main() {
     use glium::{glutin, Surface};
 
-    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
@@ -36,11 +36,14 @@ fn main() {
 
             out vec3 v_normal;
 
-            uniform mat4 matrix;
+            uniform mat4 prespective;
+            uniform mat4 view;
+            uniform mat4 model;
 
             void main () {
-                v_normal = transpose(inverse(mat3(matrix))) * normal;
-                gl_Position = matrix * vec4(position, 1.0);
+                mat4 modelview = view * model;
+                v_normal = transpose(inverse(mat3(modelview))) * normal;
+                gl_Position = prespective * modelview * vec4(position, 1.0);
             }
         "#;
     let fragment_shader_src = r#"
@@ -82,19 +85,40 @@ fn main() {
             _ => return,
         }
 
+        let mut target = display.draw();
+        target.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+
+        let perspective = {
+            let (width, height) = target.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
+
+            let fov: f32 = 3.141592 / 3.0;
+            let zfar = 1024.0;
+            let znear = 0.1;
+
+            let f = 1.0 / (fov / 2.0).tan();
+
+            [
+                [f * aspect_ratio, 0.0, 0.0, 0.0],
+                [0.0, f, 0.0, 0.0],
+                [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
+                [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
+            ]
+        };
+
         let light = [1.0, 0.4, 0.9f32];
+        let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
         let uniforms = uniform! {
-            matrix: [
+            model: [
                 [0.01, 0.0, 0.0, 0.0],
                 [0.0, 0.01, 0.0, 0.0],
                 [0.0, 0.0,  0.01,0.0],
-                [0.0, 0.0,  0.0, 1.0f32]
+                [0.0, 0.0,  2.0,   1.0f32]
             ],
+            prespective: perspective,
+            view: view,
             u_light: light
         };
-
-        let mut target = display.draw();
-        target.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
 
         let params = glium::DrawParameters {
             depth: glium::Depth {
@@ -102,6 +126,7 @@ fn main() {
                 write: true,
                 ..Default::default()
             },
+            backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
             ..Default::default()
         };
 
@@ -116,4 +141,44 @@ fn main() {
             .unwrap();
         target.finish().unwrap();
     })
+}
+
+fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [
+        up[1] * f[2] - up[2] * f[1],
+        up[2] * f[0] - up[0] * f[2],
+        up[0] * f[1] - up[1] * f[0],
+    ];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [
+        f[1] * s_norm[2] - f[2] * s_norm[1],
+        f[2] * s_norm[0] - f[0] * s_norm[2],
+        f[0] * s_norm[1] - f[1] * s_norm[0],
+    ];
+
+    let p = [
+        -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+        -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+        -position[0] * f[0] - position[1] * f[1] - position[2] * f[2],
+    ];
+
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
 }
