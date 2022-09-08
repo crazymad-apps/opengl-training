45f5d63eca70f5635bd741275a8c9e42929ef705
crazymad <2116913961@qq.com>
2022-09-08 18:03:01 +0800

    trying create a cube mesh

---
+++ src/cube.rs
@@ -0,0 +1,44 @@
+pub struct Vertex {
+    position: (f32, f32, f32),
+}
+
+pub fn cube_mesh(center: &[f32; 3]) -> (Vec<Vertex>, Vec<u16>) {
+    let a1 = Vertex {
+        position: (-0.5, 0.5, -0.5f32),
+    };
+    let b1 = Vertex {
+        position: (0.5, 0.5, -0.5f32),
+    };
+    let c1 = Vertex {
+        position: (0.5, -0.5, -0.5f32),
+    };
+    let d1 = Vertex {
+        position: (-0.5, -0.5, -0.5f32),
+    };
+
+    let a2 = Vertex {
+        position: (-0.5, 0.5, 0.5f32),
+    };
+    let b2 = Vertex {
+        position: (0.5, 0.5, 0.5f32),
+    };
+    let c2 = Vertex {
+        position: (0.5, -0.5, 0.5f32),
+    };
+    let d2 = Vertex {
+        position: (-0.5, -0.5, 0.5f32),
+    };
+
+    let indices = vec![
+        0, 1, 2, 2, 3, 0, // 正面
+        0, 3, 4, 4, 3, 7, // 左面
+        7, 6, 4, 4, 6, 5, // 背面
+        5, 1, 0, 0, 4, 5, // 上面
+        5, 6, 1, 1, 6, 2, // 右面
+        2, 6, 3, 3, 6, 7u16, // 下面
+    ];
+
+    let vertices = vec![a1, b1, c1, d1, a2, b2, c2, d2];
+
+    return (vertices, indices);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate glium;
 
+mod camera;
+mod cube;
 mod teapot;
 
 #[derive(Copy, Clone)]
@@ -19,12 +21,14 @@ fn main() {
     let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
-    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let (vertices, indices) = cube::cube_mesh(&[0.0, 0.0, 0.0f32]);
+    let _vectices = vertices.as_slice();
+
+    let positions = glium::VertexBuffer::new(&display, _vectices).unwrap();
     let indices = glium::IndexBuffer::new(
         &display,
         glium::index::PrimitiveType::TrianglesList,
-        &teapot::INDICES,
+        &indices,
     )
     .unwrap();
 
@@ -32,9 +36,6 @@ fn main() {
             #version 150      // updated
 
             in vec3 position;
-            in vec3 normal;
-
-            out vec3 v_normal;
 
             uniform mat4 prespective;
             uniform mat4 view;
@@ -42,22 +43,16 @@ fn main() {
 
             void main () {
                 mat4 modelview = view * model;
-                v_normal = transpose(inverse(mat3(modelview))) * normal;
                 gl_Position = prespective * modelview * vec4(position, 1.0);
             }
         "#;
     let fragment_shader_src = r#"
             #version 140
 
-            in vec3 v_normal;
             out vec4 color;
-            uniform vec3 u_light;
 
             void main() {
-                float brightness = dot(normalize(v_normal), normalize(u_light));
-                vec3 dark_color = vec3(0.6, 0.0, 0.0);
-                vec3 regular_color = vec3(1.0, 0.0, 0.0);
-                color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+                color = vec4(1.0, 0.0, 0.0, 1.0);
             }
         "#;
     let program =
@@ -107,7 +102,7 @@ fn main() {
         };
 
         let light = [1.0, 0.4, 0.9f32];
-        let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
+        let view = camera::view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
         let uniforms = uniform! {
             model: [
                 [0.01, 0.0, 0.0, 0.0],
@@ -131,54 +126,8 @@ fn main() {
         };
 
         target
-            .draw(
-                (&positions, &normals),
-                &indices,
-                &program,
-                &uniforms,
-                &params,
-            )
+            .draw(&positions, &indices, &program, &uniforms, &params)
             .unwrap();
         target.finish().unwrap();
     })
-}
-
-fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
-    let f = {
-        let f = direction;
-        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
-        let len = len.sqrt();
-        [f[0] / len, f[1] / len, f[2] / len]
-    };
-
-    let s = [
-        up[1] * f[2] - up[2] * f[1],
-        up[2] * f[0] - up[0] * f[2],
-        up[0] * f[1] - up[1] * f[0],
-    ];
-
-    let s_norm = {
-        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
-        let len = len.sqrt();
-        [s[0] / len, s[1] / len, s[2] / len]
-    };
-
-    let u = [
-        f[1] * s_norm[2] - f[2] * s_norm[1],
-        f[2] * s_norm[0] - f[0] * s_norm[2],
-        f[0] * s_norm[1] - f[1] * s_norm[0],
-    ];
-
-    let p = [
-        -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
-        -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
-        -position[0] * f[0] - position[1] * f[1] - position[2] * f[2],
-    ];
-
-    [
-        [s_norm[0], u[0], f[0], 0.0],
-        [s_norm[1], u[1], f[1], 0.0],
-        [s_norm[2], u[2], f[2], 0.0],
-        [p[0], p[1], p[2], 1.0],
-    ]
 }
