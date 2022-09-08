6292b6ec7cc292c12ec0e6076ecf2d4fbf2070d0
crazymad-apps <crazymad0601.apps@gmail.com>
2022-09-08 22:28:34 +0800

    光照

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,8 @@ extern crate glium;
 
 mod teapot;
 
-#[derive(Copy, Clone)]
-struct Vertex {
-    position: [f32; 2],
-    tex_coords: [f32; 2],
-}
-
-implement_vertex!(Vertex, position, tex_coords);
-
 fn main() {
+    #![allow(unused_imports)]
     use glium::{glutin, Surface};
 
     let event_loop = glutin::event_loop::EventLoop::new();
@@ -29,37 +22,43 @@ fn main() {
     .unwrap();
 
     let vertex_shader_src = r#"
-            #version 150      // updated
+        #version 150
+        in vec3 position;
+        in vec3 normal;
+        out vec3 v_normal;
+        out vec3 v_position;
+        uniform mat4 perspective;
+        uniform mat4 view;
+        uniform mat4 model;
+        void main() {
+            mat4 modelview = view * model;
+            v_normal = transpose(inverse(mat3(modelview))) * normal;
+            gl_Position = perspective * modelview * vec4(position, 1.0);
+            v_position = gl_Position.xyz / gl_Position.w;
+        }
+    "#;
 
-            in vec3 position;
-            in vec3 normal;
-
-            out vec3 v_normal;
-
-            uniform mat4 prespective;
-            uniform mat4 view;
-            uniform mat4 model;
-
-            void main () {
-                mat4 modelview = view * model;
-                v_normal = transpose(inverse(mat3(modelview))) * normal;
-                gl_Position = prespective * modelview * vec4(position, 1.0);
-            }
-        "#;
     let fragment_shader_src = r#"
-            #version 140
+        #version 150
+        in vec3 v_normal;
+        in vec3 v_position;
+        out vec4 color;
 
-            in vec3 v_normal;
-            out vec4 color;
-            uniform vec3 u_light;
+        uniform vec3 u_light;
 
-            void main() {
-                float brightness = dot(normalize(v_normal), normalize(u_light));
-                vec3 dark_color = vec3(0.6, 0.0, 0.0);
-                vec3 regular_color = vec3(1.0, 0.0, 0.0);
-                color = vec4(mix(dark_color, regular_color, brightness), 1.0);
-            }
-        "#;
+        const vec3 ambient_color = vec3(0.2, 0.0, 0.0);
+        const vec3 diffuse_color = vec3(0.6, 0.0, 0.0);
+        const vec3 specular_color = vec3(1.0, 1.0, 1.0);
+
+        void main() {
+            float diffuse = max(dot(normalize(v_normal), normalize(u_light)), 0.0);
+            vec3 camera_dir = normalize(-v_position);
+            vec3 half_direction = normalize(normalize(u_light) + camera_dir);
+            float specular = pow(max(dot(half_direction, normalize(v_normal)), 0.0), 16.0);
+            color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
+        }
+    "#;
+
     let program =
         glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
             .unwrap();
@@ -88,6 +87,15 @@ fn main() {
         let mut target = display.draw();
         target.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
 
+        let model = [
+            [0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 2.0, 1.0f32],
+        ];
+
+        let view = view_matrix(&[2.0, 1.0, 1.0], &[-2.0, -1.0, 1.0], &[0.0, 1.0, 0.0]);
+
         let perspective = {
             let (width, height) = target.get_dimensions();
             let aspect_ratio = height as f32 / width as f32;
@@ -106,19 +114,7 @@ fn main() {
             ]
         };
 
-        let light = [1.0, 0.4, 0.9f32];
-        let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
-        let uniforms = uniform! {
-            model: [
-                [0.01, 0.0, 0.0, 0.0],
-                [0.0, 0.01, 0.0, 0.0],
-                [0.0, 0.0,  0.01,0.0],
-                [0.0, 0.0,  2.0,   1.0f32]
-            ],
-            prespective: perspective,
-            view: view,
-            u_light: light
-        };
+        let light = [1.4, 0.4, -0.7f32];
 
         let params = glium::DrawParameters {
             depth: glium::Depth {
@@ -126,7 +122,7 @@ fn main() {
                 write: true,
                 ..Default::default()
             },
-            backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
+            //backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockWise,
             ..Default::default()
         };
 
@@ -135,12 +131,12 @@ fn main() {
                 (&positions, &normals),
                 &indices,
                 &program,
-                &uniforms,
+                &uniform! { model: model, view: view, perspective: perspective, u_light: light },
                 &params,
             )
             .unwrap();
         target.finish().unwrap();
-    })
+    });
 }
 
 fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
